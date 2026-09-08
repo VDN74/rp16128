5c3cf9402ea99e2f473deec891a67500da095401
VDN74 <vasilevdn74@yandex.ru>
2026-09-09 00:38:11 +0300

    2Modal is OK

--- src/App.jsx
+++ src/App.jsx
@@ -42,8 +42,8 @@ const App = () => {
       display: "flex",
       flexDirection: "column",
       gap: "10px",
-      width: "580px",
-      height: "310px",
+      width: "585px",
+      height: "315px",
       zIndex: "10",
     },
     overlay: {
